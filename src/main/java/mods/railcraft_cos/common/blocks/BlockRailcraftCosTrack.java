a346e1bad1e051d0268dcefafed3f15715f444f5
Hanakocz <jujky.zon@gmail.com>
2016-04-26 22:12:31 +0200

    Grass Rail can now be sheared by Shears (rightclick)  - Rail reverted to vanilla one  - Grass dropped as when normal grass block is broken by shears

--- src/main/java/mods/railcraft_cos/common/blocks/BlockRailcraftCosTrack.java
+++ src/main/java/mods/railcraft_cos/common/blocks/BlockRailcraftCosTrack.java
@@ -7,11 +7,18 @@ import cpw.mods.fml.relauncher.SideOnly;
 import mods.railcraft_cos.common.core.ClientProxy;
 import mods.railcraft_cos.common.core.Railcraft_Cos;
 import net.minecraft.block.BlockRailBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemShears;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
-public class BlockRailcraftCosTrack extends BlockRailBase {
+public class BlockRailcraftCosTrack extends BlockRailBase
+{
 	
-	public BlockRailcraftCosTrack(String unlocalizedname, boolean b) {
+	public BlockRailcraftCosTrack(String unlocalizedname, boolean b) 
+	{
 		super(false);
 		setBlockName(unlocalizedname);
 		setBlockTextureName(Railcraft_Cos.MODID + ":" + "track.grass");
@@ -19,7 +26,8 @@ public class BlockRailcraftCosTrack extends BlockRailBase {
 	}
 	
 	@Override
-	public Item getItemDropped(int metadata, Random random, int fortune) {
+	public Item getItemDropped(int metadata, Random random, int fortune) 
+	{
 		return Item.getItemFromBlock(this);
 	}
 	
@@ -29,5 +37,32 @@ public class BlockRailcraftCosTrack extends BlockRailBase {
         return ClientProxy.railcraftCosTrackRenderer;
     }
 	
-	
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int meta, float sideX, float sideY, float sideZ)
+    {
+		if(player.getCurrentEquippedItem() != null)
+		{
+			ItemStack item = player.getCurrentEquippedItem();
+
+			if(item.getItem() instanceof ItemShears)
+			{
+				int oldmeta = world.getBlockMetadata(x, y, z);
+				world.setBlock(x, y, z, Blocks.rail, oldmeta, 2);
+				ItemStack itemstack = new ItemStack(Blocks.tallgrass, 1, 1);
+				dropBlockAsItem(world, x, y, z, itemstack);
+				item.damageItem(1, player);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+		else
+		{
+			return false;
+		}		
+    }
+
 }
